1d4e804079696dfa8a4cef84fa7c53f429c37ac9
PedroPini <augustopini@gmail.com>
2019-10-05 12:35:34 -0300

    adicionando Funcionalidades do customer

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customer
         public ActionResult Index()
         {
-            return View();
+            var customers = _context.Customers.ToList();
+            
+            return View(customers);
         }
+
+        public ActionResult Details(int id)
+        {
+            var customers = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customers == null)
+                return HttpNotFound();
+            return View(customers);
+        }
+
     }
 }
