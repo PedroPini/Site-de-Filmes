da186627f1a9883145e27e0c0e142a1f793dc1b4
PedroPini <augustopini@gmail.com>
2019-10-05 08:36:38 -0300

    Adicionando projeto inicial

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            //Codigo fragil nao se atualiza conforme é editado o nome de uma funcao no controller
+            //routes.MapRoute("MoviesByReleaseDate",
+            //                "movies/released/{year}/{month}",
+            //                new { controller = "Movies", action = "ByReleaseDate"},
+            //                new { year = @"\d{4}", month = @"\d{2}"});
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Models/MyDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
